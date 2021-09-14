7d6d9525d17295daec2d3ea1f8df440b3079468c
raykovski <44312300+raykovski@users.noreply.github.com>
2021-09-14 22:53:52 +0300

    Add files via upload

---
+++ functions-101.js
@@ -0,0 +1,17 @@
+
+
+let square = function (num) {
+    let result = num * num
+    console.log(result)
+    return result
+}
+
+square(3)
+
+square(10)
+//let value = square(3)
+//let otherValue = square(10)
+
+//console.log(value)
+//console.log(otherValue)
+
